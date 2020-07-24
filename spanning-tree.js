0531cf92e4c1e97171d717dfcff3cfaf3e77cf91
sharpara2105 <sharpara.2105@gmail.com>
2020-07-24 09:24:37 +0530

    minheap creation and extracting first smallest element

--- spanning-tree.js
+++ spanning-tree.js
@@ -296,43 +296,6 @@ var sourceEdge = findMin(edgeLength);
     */
 
 
-//finding minimum element by creating a min heap and extracting the first element.
-//let's create a min heap:
-/*
-lengthArray = [5,4,8,7,6,2,1]
-
-function minHeapify(structure,i) {
-    var smallest = i;
-    var l = 2*i+1 ;
-    var r = 2*i+2;
-    if (l < structure.length && structure[l]<structure[i]) {
-        smallest = l;
-    }
-    if( l < structure.length && structure[r]<structure[smallest]) {
-        smallest = r;
-    }
-    if(smallest != i) {
-    //swap structure[i] with structure[smallest]
-    var temp = structure[i];
-    structure[i] = structure[smallest];
-    structure[smallest] = temp;
-    // i= smallest;
-    minHeapify(lengthArray,smallest);
-    }
-    return structure;
-
-}
-
-function buildMinheap() {
-    var heapSize = lengthArray.length;
-    for (var i = Math.floor(heapSize/2)-1; i >= 0 ; i--) {      
-       var comp= minHeapify(lengthArray,i);
-    }
-    console.log(comp);
-}
-buildMinheap();
-*/
-
 
 
 
